d5e52383517863df2a799990008454a184c4e1d3
Sam Cunliffe <sam.cunliffe@gmail.com>
2012-06-14 09:16:23 +0100

    Added readme, fixed another input.

--- 042.py
+++ 042.py
@@ -1,22 +1,15 @@
-# Project Euler Problem 42
-# ========================
-# The n^(th) term of the sequence of triangle numbers
-# is given by, t_(n) = 1/2n(n+1); so the first ten 
-# triangle numbers are:
-#       1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
-
-# By converting each letter in a word to a number 
-# corresponding to its alphabetical position and
-# adding these values we form a word value. For
-# example, the word value for SKY is 
-#      19 + 11 + 25 = 55 = t_(10). 
-# If the word value is a triangle number then we
-# shall call the word a triangle word.
-
-# Using words.txt (right click and 'Save 
-# Link/Target As...'), a 16K text file containing 
-# nearly two-thousand common English words, how many
-# are triangle words?
+# The n^(th) term of the sequence of triangle numbers is given by,
+#                    t_(n) = 1/2n(n+1);
+# so the first ten triangle numbers are: 
+#            1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...  
+# By converting each letter in a word to a number corresponding to its
+# alphabetical position and # adding these values we form a word value. For 
+# example, the word value for SKY is: 19 + 11 + 25 = 55 = t_(10).  If the word
+# value is a triangle number then we # shall call the word a triangle word.
+#
+# Using words.txt (right click and 'Save Link/Target As...'), a 16K text file
+# containing nearly two-thousand common English words, how many are triangle
+# words?
 
 
 def tN(n): 
@@ -24,14 +17,12 @@ def tN(n):
 
 
 
-f=open("problem42data.txt","r") # opens the file
-# the file is now in variable f
+f=open("inputs/words.txt","r") 
 w=f.read() # puts the file contents as a string 
 w=eval(w)  # makes the file contents into a list
 
 # calculate a big old list of triangle numbers
 triNums=map(tN,range(1,10001))
-print triNums
 
 # a negative number to be added to ord(letter)
 # to make A==1, B==2 etc
@@ -39,25 +30,18 @@ offset=1-ord('A')
 
 # counters
 nTriWords=0
-no=0
 
 # loop over words
 for i in range(0,len(w)):
    word=w[i]
-   print word
    t=0
    
    # calculate current word total==t
    for j in range(0,len(word)):
       t+=(ord(word[j])+offset)
-   print t
-   print "--"
 
    # is t a triangle number?
    if triNums.count(t)==1:
       nTriWords+=1
-   elif triNums.count(t)!=1:
-      no+=1
 
 print nTriWords
-print no
