e69f051793ee03860f69a6208a5ebc8a5c77209c
sam <sam@zero-gravitas>
2015-10-02 17:32:06 +0100

    Blisteringly fast solution from the haskell wiki

--- haskell/003.hs
+++ haskell/003.hs
@@ -2,8 +2,8 @@ import Primes (primesUpto)
 
 main = do
     print(primeFactors 13195)
-    print(head(primeFactors 600851475143))
+    print(last(primeFactors 600851475143))
 
 primeFactors :: Integer -> [Integer]
-primeFactors n = [ x | x <- rev_primes, n `mod` x == 0 ]
-    where rev_primes = reverse $ primesUpto $ floor $ sqrt $ fromIntegral n
+primeFactors n = [ x | x <- relavent_primes, n `mod` x == 0 ]
+    where relavent_primes = primesUpto $ floor $ sqrt $ fromIntegral n
