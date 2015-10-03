86dcc7771745d8971aaf7710f81ad029020fda47
sam <sam@zero-gravitas>
2015-10-03 03:05:47 +0100

    Rearranged primes functions
    I need prime factor finding function for problem 5, I think.

--- haskell/Primes.hs
+++ haskell/Primes.hs
@@ -1,5 +1,5 @@
 
-module Primes (primes, primesUpto) where
+module Primes (primes, primesUpto, primeFactors) where
 
 -- infinite list of primes using Eratosthenes sieve
 primes = 2 : sieve [3,5..]
@@ -10,3 +10,19 @@ primesUpto n = takeWhile (<n) primes
 
 -- only sieves odd numbers with recursive sieve definition, filtering the further list by testing every element does not divide any element so far
 
+-- this way is slow - first attempt
+primeFactors' :: Integer -> [Integer]
+primeFactors' n = [ x | x <- relavent_primes, n `mod` x == 0 ]
+    where relavent_primes = primesUpto $ floor $ sqrt $ fromIntegral n
+
+-- this way is taken from haskell wiki
+--    https://wiki.haskell.org/Euler_problems/1_to_10#Problem_3 
+--
+-- recursive definition of factors which divides the number by it's prime factor each time
+primeFactors :: Integer -> [Integer]
+primeFactors n = factors n primes
+    where
+        factors n (p:ps)
+            | p*p > n        = [n]                            -- termination condition
+            | n `mod` p == 0 = p : factors (n `div` p) (p:ps) -- if we find a factor 
+            | otherwise      = factors n ps                   -- if we don't 
